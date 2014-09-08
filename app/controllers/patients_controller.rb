dc133fde31eff1a6b685f2c8af376d30b601c25f
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-08 12:39:17 -0600

    Fixing mistakes

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,6 +3,7 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.find(params[:id])
     @medications = Medication.all
+    @prescriptions = Prescription.all
   end
 
 end
