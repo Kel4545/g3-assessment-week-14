bbf03fc763b084dabf2fca74cafcc234892cbd0e
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-08 12:52:48 -0600

    Working on fixing mistakes

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -6,4 +6,8 @@ validates :dosage, presence: true
 validates :schedule, presence: true
 validates :starts_on, presence: true
 validates :ends_on, presence: true
+
+def medication_name(id)
+  Medication.find(id).name
+end
 end
