6b0abfa7b7eb27f005219771bece1c08b1a8043e
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-08 12:30:37 -0600

    Correcting mistakes

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -12,8 +12,8 @@ class PrescriptionsController < ApplicationController
   end
 
   def create
-    medications_params = params.require(:name).permit(:name, :dosage, :schedule, :starts_on, :ends_on).merge(patient_id: params[:patient_id])
-    @prescriptions = Medication.new(medications_params)
+    prescriptions_params = params.require(:name).permit(:name, :dosage, :schedule, :starts_on, :ends_on).merge(patient_id: params[:patient_id])
+    @prescriptions = Medication.new(prescriptions_params)
     if @prescriptions.save
       redirect_to patient_prescription_path, notice: "Your prescription was created successfully!"
     else
