a8f9ca19c397fac389b8ee5d9dbb16ba73d7aebc
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-08 13:02:12 -0600

    Fixing associations

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -2,5 +2,7 @@ class MedicationsController < ApplicationController
 
   def show
     @medications = Medications.find(params[:id])
+    @prescriptions = Prescription.all
+    @patients = Patient.all
   end
 end
