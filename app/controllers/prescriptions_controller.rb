4584b618b64fc187283c9c98a93dbcf2d7f20780
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-08 13:11:27 -0600

    Fixed associations

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -2,13 +2,13 @@ class PrescriptionsController < ApplicationController
 
   def new
     @patients = Patient.find(params[:patient_id])
-    @prescriptions = Prescription.find(params[:id])
+    @prescriptions = Prescription.new
     @medications = Medication.all
   end
 
 
   def create
-    prescriptions_params = params.require(:name).permit(:name, :dosage, :schedule, :starts_on, :ends_on).merge(patient_id: params[:patient_id])
+    prescriptions_params = params.require(:dosage).permit(:medication_id, :schedule, :starts_on, :ends_on).merge(patient_id: params[:patient_id])
     @prescriptions = Medication.new(prescriptions_params)
     if @prescriptions.save
       redirect_to patient_prescription_path, notice: "Your prescription was created successfully!"
