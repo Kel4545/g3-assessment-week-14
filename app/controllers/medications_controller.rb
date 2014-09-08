61a86702065c03473ca36d33955cf70fe4457f26
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-08 13:05:19 -0600

    Fixed associations

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -2,7 +2,5 @@ class MedicationsController < ApplicationController
 
   def show
     @medications = Medications.find(params[:id])
-    @prescriptions = Prescription.all
-    @patients = Patient.all
   end
 end
